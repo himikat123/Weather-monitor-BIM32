48d9e1772ec2be64b0cc862773d329e7d5c98f07
himikat123 <himikat123@gmail.com>
2023-01-04 11:40:04 +0100

    v3.3

--- BIM32/BIM32.ino
+++ BIM32/BIM32.ino
@@ -1,8 +1,8 @@
 /**
- *  Weather Monitor BIM32 v3.2
+ *  Weather Monitor BIM32 v3.3
  *  https://github.com/himikat123/Weather-monitor-BIM32
 
- *  © himikat123@gmail.com, Nürnberg, Deutschland, 2020-2022
+ *  © himikat123@gmail.com, Nürnberg, Deutschland, 2020-2023
 
  *  ESP32 Dev Module
  *  1.2 MB APP / 1.5 MB SPIFFS
@@ -102,7 +102,7 @@ void setup() {
   myNex.writeStr("page Logo");
   Serial.println(SEPARATOR);
   Serial.println(SEPARATOR);
-  Serial.println("*  Weather Monitor BIM32 " + String(global.fw) + "    © himikat123@gmail.com   2020-2022  *");
+  Serial.println("*  Weather Monitor BIM32 " + String(global.fw) + "    © himikat123@gmail.com   2020-2023  *");
   Serial.println(SEPARATOR);
   Serial.println();
 
