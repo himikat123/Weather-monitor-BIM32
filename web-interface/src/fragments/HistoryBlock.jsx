17d07c3d7e9e984854ae5db8de819a37d894f6e2
himikat123 <himikat123@gmail.com>
2023-01-14 17:16:52 +0100

    mobile view optimization

--- web-interface/src/fragments/HistoryBlock.jsx
+++ web-interface/src/fragments/HistoryBlock.jsx
@@ -5,6 +5,26 @@ import Languages from "../Languages";
 class HistoryBlock extends React.Component {
     constructor(props) {
         super(props);
+
+        this.state = {
+            width: 250
+        }
+
+        this.setChartWidth = this.setChartWidth.bind(this);
+    }
+
+    setChartWidth(width) {
+        if(width > 550) width = 450;
+        else width -= 100;
+        this.setState({width: width});
+    }
+
+    componentDidMount() {
+        this.setChartWidth(document.querySelector('.thchart').offsetWidth);
+
+        window.addEventListener('resize', () => {
+            this.setChartWidth(document.querySelector('.thchart').offsetWidth);
+        });
     }
 
     render() {
@@ -12,13 +32,13 @@ class HistoryBlock extends React.Component {
         let lang = this.props.config.lang;
 
         return(
-            <div className="col p-4">
+            <div className="col col-12 col-lg-6 p-4 thchart">
                 <div className="border border-secondary rounded p-3 h-100 sensor">
                     <div className="w-100 text-center">
-                        <iframe width="450" 
+                        <iframe width={this.state.width} 
                             height="261" 
                             style={{border: '1px solid #cccccc'}} 
-                            src={`https://thingspeak.com/channels/${this.props.config.history.channelID}/charts/${Number(this.props.num)+1}?bgcolor=%23${this.props.bgcolor}&color=%23${this.props.color}&dynamic=true&results=24&round=2&title=${this.props.fields}&type=line&api_key=${this.props.config.history.rdkey}`}>
+                            src={`https://thingspeak.com/channels/${this.props.config.history.channelID}/charts/${Number(this.props.num)+1}?bgcolor=%23${this.props.bgcolor}&color=%23${this.props.color}&dynamic=true&results=24&round=2&title=${this.props.fields}&type=line&api_key=${this.props.config.history.rdkey}&width=${this.state.width}`}>
                         </iframe>
                     </div>
                     <SelectInput value={`history.fields.${this.props.num}`}
