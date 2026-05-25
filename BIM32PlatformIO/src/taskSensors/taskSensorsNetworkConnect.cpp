071e8080f165fb76486825705338341c83132327
himikat123 <himikat123@gmail.com>
2026-05-25 12:12:38 +0200

    Do not attempt to connect to the network while the access point is active

--- BIM32PlatformIO/src/taskSensors/taskSensorsNetworkConnect.cpp
+++ BIM32PlatformIO/src/taskSensors/taskSensorsNetworkConnect.cpp
@@ -8,7 +8,7 @@
  * Network connection if not connected and if not Access point mode
  */
 void TaskSensors::_networkConnect() {
-    if(WiFi.localIP().toString() == "0.0.0.0" or !network.isConnected()) {
+    if(!state.apMode && (WiFi.localIP().toString() == "0.0.0.0" or !network.isConnected())) {
         state.net_connected = false;
         network.connect();
         vTaskDelay(1000);
