ac88c3d04a5dce5338b19a05da5fa3274f6cfe0d
himikat123 <himikat123@gmail.com>
2026-05-29 11:43:35 +0200

    ntp sync

--- BIM32PlatformIO/src/timeNTP/timeNTP.cpp
+++ BIM32PlatformIO/src/timeNTP/timeNTP.cpp
@@ -1,5 +1,5 @@
 #include <Arduino.h>
-#include <TimeLib.h> // v1.6.1 https://playground.arduino.cc/Code/Time/
+#include <TimeLib.h>
 
 #include "./timeNTP.hpp"
 #include "../state/state.hpp"
@@ -13,21 +13,20 @@
 void TimeNTP::syncNTP(void) {
     if(state.net_connected) {
         configTime(config.clock.utc() * 3600, 0, config.clock.ntp());
+        vTaskDelay(pdMS_TO_TICKS(2000));
+
         struct tm tmstruct;
-        vTaskDelay(1000);
-        tmstruct.tm_year = 0;
-        getLocalTime(&tmstruct);
-        if(tmstruct.tm_year >= 121) {
+        if(getLocalTime(&tmstruct)) {
             setTime(tmstruct.tm_hour, tmstruct.tm_min, tmstruct.tm_sec, tmstruct.tm_mday, tmstruct.tm_mon + 1, tmstruct.tm_year + 1900);
-            unsigned int summertime = config.clock.dlst() ? isSummertime() ? 3600 : 0 : 0;
-            unsigned int t = now() + summertime;
-            setTime(t);
+            if(config.clock.dlst() && isSummertime()) adjustTime(3600);
+
             state.clockSynchronize = true;
             state.clockSynchronized = true;
+
             nextion.setDisplayRTC();
             sensors.set_ds3231_timeDate();
-            Serial.print("successful: ");
-            Serial.printf("%d:%02d:%02d %02d.%02d.%d\r\n", hour(), minute(), second(), day(), month(), year());
+
+            Serial.printf("successful: %02d:%02d:%02d %02d.%02d.%d\r\n", hour(), minute(), second(), day(), month(), year());
         }
         else {
             Serial.println("failed");
@@ -41,11 +40,23 @@ void TimeNTP::syncNTP(void) {
  * Check the time and date for daylight saving time
  */
 boolean TimeNTP::isSummertime() {
-    if(month() < 3 || month() > 10) return false;
-    if(month() > 3 && month() < 10) return true;
-    if(
-        (month() == 3 && (hour() + 24 * day()) >= (1 + 24 * (31 - (5 * year() / 4 + 4) % 7))) || 
-        (month() == 10 && (hour() + 24 * day()) < (1 + 24 * (31 - (5 * year() / 4 + 1) % 7)))
-    ) return true;
-    else return false;
+    int y = year(), m = month(), d = day(), h = hour();
+
+    if(m < 3 || m > 10) return false;
+    if(m > 3 && m < 10) return true;
+    
+    int lastMarchSunday = 31 - ((5 * y / 4 + 4) % 7);
+    int lastOctoberSunday = 31 - ((5 * y / 4 + 1) % 7);
+    
+    if(m == 3) {
+        if(d > lastMarchSunday || (d == lastMarchSunday && h >= 2)) return true;
+        return false;
+    }
+
+    if(m == 10) {
+        if(d < lastOctoberSunday || (d == lastOctoberSunday && h < 3)) return true;
+        return false;
+    }
+
+    return false;
 }
