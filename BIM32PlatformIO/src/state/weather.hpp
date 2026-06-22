e0457e30bd3147c30dd61653a89fdb1e242d1fba
himikat123 <himikat123@gmail.com>
2026-06-22 15:30:28 +0200

    add hourly weather data to the weather json

--- BIM32PlatformIO/src/state/weather.hpp
+++ BIM32PlatformIO/src/state/weather.hpp
@@ -36,7 +36,7 @@ struct WeatherState {
             JsonArray a_wind = o["wind"].to<JsonArray>();
             JsonArray a_icon = o["icon"].to<JsonArray>();
             
-            for(int i=0; i<5; i++){
+            for(int i=0; i<5; i++) {
                 a_tMax.add(tMax[i]);
                 a_tMin.add(tMin[i]);
                 a_wind.add(wind[i]);
@@ -54,6 +54,26 @@ struct WeatherState {
         int windDir[HOURLY_COUNT] = { 0 };
         float prec[HOURLY_COUNT] = { 0 };
         time_t time = 0;
+
+        void toJson(JsonObject o) const {
+            JsonArray a_date = o["date"].to<JsonArray>();
+            JsonArray a_icon = o["icon"].to<JsonArray>();
+            JsonArray a_temp = o["temp"].to<JsonArray>();
+            JsonArray a_pres = o["pres"].to<JsonArray>();
+            JsonArray a_windSpeed = o["windSpeed"].to<JsonArray>();
+            JsonArray a_windDir = o["windDir"].to<JsonArray>();
+            JsonArray a_prec = o["prec"].to<JsonArray>();
+            
+            for(int i=0; i<HOURLY_COUNT; i++) {
+                a_date.add(date[i]);
+                a_icon.add(icon[i]);
+                a_temp.add(temp[i]);
+                a_pres.add(pres[i]);
+                a_windSpeed.add(windSpeed[i]);
+                a_windDir.add(windDir[i]);
+                a_prec.add(prec[i]);
+            }
+        }
     } hourly;
 
     void toJson(JsonObject obj) const {
@@ -71,5 +91,8 @@ struct WeatherState {
 
         JsonObject dailyObj = o["daily"].to<JsonObject>();
         daily.toJson(dailyObj);
+
+        JsonObject hourlyObj = o["hourly"].to<JsonObject>();
+        hourly.toJson(hourlyObj);
     }
 };
