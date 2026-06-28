98930c132a6dd523a5e89c73c95648f67fa2ad10
himikat123 <himikat123@gmail.com>
2026-06-28 18:45:10 +0200

    fix wind speed from wireless sensor

--- BIM32PlatformIO/src/wsensor/receive.cpp
+++ BIM32PlatformIO/src/wsensor/receive.cpp
@@ -12,6 +12,7 @@ void WirelessSensor::receive() {
     String wsensorStr = "";
     char lastChar = _receivedData[_receivedIndex];
     char last_char = _receivedData[_receivedIndex - 1];
+
     if(lastChar == '\n' or lastChar == '}' or last_char == '\n' or last_char == '}') {
         _receivedIndex = 0;
         Serial.println(SEPARATOR);
@@ -21,42 +22,53 @@ void WirelessSensor::receive() {
         int index = received.indexOf('\n');
         wsensorStr = received.substring(0, index);
         Serial.println(wsensorStr);
+
         for(uint8_t i=0; i<3; i++) {
             int startJson = wsensorStr.indexOf("{");
             int endJson = wsensorStr.indexOf("}");
+
             if(startJson != -1 and endJson != -1) {
                 JsonDocument root;
                 String js = wsensorStr.substring(startJson, endJson + 1);
                 wsensorStr = wsensorStr.substring(endJson + 1);
+
                 DeserializationError error = deserializeJson(root, js);
                 if(error) {
                     Serial.println("Wireless sensor deserialization error");
                     return;
                 }
+
                 int number = root["num"];
                 if(number >= 0 and number < WSENSORS) {
                     state.wsensor.time[number] = now();
+
                     // temperature
                     state.wsensor.temp.data[0][number] = root["t"] | 40400.0;
                     strlcpy(state.wsensor.temp.name[0][number], root["s"] | "", sizeof(state.wsensor.temp.name[0][number]));
+
                     for(int i=1; i<5; i++) {
                         state.wsensor.temp.data[i][number] = root["ds"][i - 1] | 40400.0;
                         strlcpy(state.wsensor.temp.name[i][number], "DS18B20", sizeof(state.wsensor.temp.name[i][number]));
                     }
+
                     // humidity
                     state.wsensor.hum.data[number] = root["h"] | 40400.0;
                     strlcpy(state.wsensor.hum.name[number], root["s"] | "", sizeof(state.wsensor.hum.name[number]));
                     // pressure
+
                     state.wsensor.pres.data[number] = root["p"] | 40400.0;
                     strlcpy(state.wsensor.pres.name[number], root["s"] | "", sizeof(state.wsensor.pres.name[number]));
+
                     // wind
-                    state.wsensor.wind.speed.data[number] = root["wind"][0] | -1;
+                    state.wsensor.wind.speed.data[number] = root["wind"][0] | -1.0;
                     strlcpy(state.wsensor.wind.speed.name[number], "RS485", sizeof(state.wsensor.wind.speed.name[number]));
-                    state.wsensor.wind.dir.data[number] = root["wind"][1] | -1;
+                    state.wsensor.wind.dir.data[number] = root["wind"][1] | -1.0;
                     strlcpy(state.wsensor.wind.dir.name[number], "RS485", sizeof(state.wsensor.wind.dir.name[number]));
+
                     // ambient light
                     state.wsensor.light.data[number] = root["l"] | -1.0;
                     strlcpy(state.wsensor.light.name[number], root["a"] | "", sizeof(state.wsensor.light.name[number]));
+
                     // PZEM-004t
                     state.wsensor.voltage.data[number] = root["pzem"][0] | -1.0;
                     strlcpy(state.wsensor.voltage.name[number], "PZEM-004t", sizeof(state.wsensor.voltage.name[number]));
@@ -68,9 +80,11 @@ void WirelessSensor::receive() {
                     strlcpy(state.wsensor.energy.name[number], "PZEM-004t", sizeof(state.wsensor.energy.name[number]));
                     state.wsensor.freq.data[number] = root["pzem"][4] | -1.0;
                     strlcpy(state.wsensor.freq.name[number], "PZEM-004t", sizeof(state.wsensor.freq.name[number]));
+
                     // Senseair S8
                     state.wsensor.co2.data[number] = root["s8"] | -1.0;
                     strlcpy(state.wsensor.co2.name[number], "Senseair S8", sizeof(state.wsensor.co2.name[number]));
+
                     // Battery
                     state.wsensor.battery.adc[number] = root["b"] | -1;
                     state.wsensor.battery.voltage[number] = (float)state.wsensor.battery.adc[number] / (300.0 - config.wsensor.batK(number));
